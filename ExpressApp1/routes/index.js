079d68b5c584b5b6123b1428b80ebfa387988368
shoredata <31630233+shoredata@users.noreply.github.com>
2019-04-06 15:43:41 -0700

    Initial commit to test migration of az deployment across platforms and repos.

---
+++ ExpressApp1/routes/index.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+// item4 is for statup instead of broken image placeholder, also when image is clicked on
+var getData = function () {
+  var data = {
+    'item1': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-76.jpg',
+    'item2': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-77.jpg',
+    'item3': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-78.jpg',
+    'item4': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-72.jpg'
+  }
+  return data;
+}
+
+
+/* GET home page. */
+//router.get('/', function (req, res) {
+//    res.render('index', { title: 'Express' });
+//});
+var requests = 0;
+router.get('/', function (req, res) {
+  requests++;
+  console.log("\r\nRequests:", requests, " URL:", req.url, "method:", req.method);
+  res.render('index', {
+    title: 'Express - ExpressApp1 via Visual Studio 2015, Javascript Node Express 4 Template for Azure', "data": getData()
+  });
+});
+
+module.exports = router;
